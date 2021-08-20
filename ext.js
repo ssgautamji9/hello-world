cf268bf429219a62e4b40438b990e6100e78a041
Shivam Gautam <30316310+ssgautamji9@users.noreply.github.com>
2021-08-20 13:47:50 +0530

    Create ext.js

---
+++ ext.js
@@ -0,0 +1,18 @@
+var is_triggered = false; 
+var my_trigger_price = 5.1;
+function make_my_profit(){
+    if(is_triggered === false)
+    {
+        var mkprice  = document.getElementById("mkprice").innerHTML;
+        mkprice = parseFloat(mkprice.replace(/[^0-9.]/g, ''));
+        if(mkprice <= my_trigger_price )
+        {
+            var ipshare = document.getElementById('inputShare');
+            ipshare.focus();
+            ipshare.value = 3;
+            console.log('Order Executed as you set.');
+            alert("Parameter matched.");
+            is_triggered = true;
+        }
+    }
+}
